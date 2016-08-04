ab7fded685dfe66105d843f04db88a159e0751e9
Soleny Abadia Rodrigues de Assis <soleny.assis@prodest.es.gov.br.local>
2016-08-04 11:31:17 -0300

    Commit inicial

---
+++ Exemplo/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exemplo.Controllers
+{
+    public class codDelegate
+    {
+
+        delegate int Operacao(int a, int b);
+        static void Main(string[] args);
+
+    }
+
+    class CodDelegate
+    {
+
+        delegate int Operacao(int a, int b, int c, int d);
+        static void Main(string[] args)
+
+        {
+
+            Operacao op = (a, b, c, d) => (a + b + c + d)  *3;            
+            Console.WriteLine  (op (2 + 4 + 5 + 6) * 3);
+            Console.ReadLine();
+
+        }
+
+    }
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Exemplo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Exemplo.Startup))]
+namespace Exemplo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
